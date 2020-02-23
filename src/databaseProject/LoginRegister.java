e2ef1a4ec66a5b6014db3aa43a0102a9a110ec0c
Santhosh <fv3517@wayne.edu>
2020-02-23 17:19:08 -0500

    fixed some stuff

--- src/databaseProject/LoginRegister.java
+++ src/databaseProject/LoginRegister.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LoginRegister
  */
-@WebServlet("/loginRegister")
+@WebServlet("/LoginRegister")
 public class LoginRegister extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
